b6d0e895142b746dab5623b3de674e1034711461
Conrad Twizzle <conrad.twizzle@gmail.com>
2013-02-11 22:25:39 -0800

    campaign updates

--- db/migrate/20130212014035_create_campaigns.rb
+++ db/migrate/20130212014035_create_campaigns.rb
@@ -1,9 +1,13 @@
 class CreateCampaigns < ActiveRecord::Migration
-  def change
+  def up
     create_table :campaigns do |t|
       t.string :title
       t.string :body
       t.timestamps
     end
+  end
+
+  def down
+
   end
 end
